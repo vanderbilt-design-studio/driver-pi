44cc7abcae6185965e731f3611f48316ff428039
Sameer Puri <sameer@users.noreply.github.com>
2019-08-29 15:30:03 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+server_address = "vanderbilt-design-studio.herokuapp.com"
+server_port = 443
+server_uri = "wss://{}:{}".format(server_address, server_port)
+logging_format = '[%(asctime)s] %(levelname)s: %(message)s'
---
+++ main.py
@@ -0,0 +1,36 @@
+import asyncio
+import ssl
+import websockets
+import json
+import pprint
+import wiringpi
+
+from typing import Dict, List
+import socket
+
+from config import logging_format, server_uri
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO, format=logging_format)
+
+ssl_context = ssl.create_default_context()
+
+wiringpi.wiringPiSetupGpio()
+wiringpi.pinMode(22, wiringpi.OUTPUT)
+
+async def recv_status():
+    async with websockets.connect(server_uri + '/sign', ssl=ssl_context) as websocket:
+        while True:
+            msg = await websocket.recv()
+            try:
+                msg_json = json.loads(msg)
+                wiringpi.digitalWrite(22, 1 if msg_json['open'] else 0)
+            except:
+                continue
+
+while True:
+    try:
+        asyncio.get_event_loop().run_until_complete(recv_status())
+    except Exception as serr:
+        logging.warning(f"Exception while receiving status, attempting to start again: {serr}")
